386006ec46823cc05950a387ebc65f7ece1c89c7
juancolson <87849544+juancolson@users.noreply.github.com>
2021-09-14 10:47:41 +0545

    Update searcher.py

--- Main/searcher.py
+++ Main/searcher.py
@@ -1,6 +1,7 @@
 import cfscrape
 import bs4
 import logging
+from Main.detail import get_detail
 
 def find_light_novel(title):
     scrape = cfscrape.create_scraper()
@@ -9,17 +10,20 @@ def find_light_novel(title):
     page = scrape.get(lndb_info_query,
                         headers={"connection": "keep-alive", "host": "lndb.info"})
     search = bs4.BeautifulSoup(page.content, "html.parser")
-    light_novel_list = search.find("div", {"id": "bodylightnovelscontentid"})
-    light_novels = light_novel_list.findAll('a')
 
-    search_result = []
+    if "search?text=" in page.url:
+        light_novel_list = search.find("div", {"id": "bodylightnovelscontentid"})
+        light_novels = light_novel_list.findAll('a')
 
-    for individual in light_novels:
-        item = {
-            'title': individual.text.strip(),
-            'lndb': individual['href'],
-        }
-        search_result.append(item)
+        search_result = []
 
+        for individual in light_novels:
+            item = {
+                'title': individual.text.strip(),
+                'lndb': individual['href'],
+            }
+            search_result.append(item)
+    else:
+        search_result = get_detail(page.url)
     return search_result
     
